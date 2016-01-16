b27ecb39a497ea6106a13e385067f3cf7a17d712
jeremynealbrown <jeremynealbrown@gmail.com>
2016-01-15 16:06:36 -0800

    Made path to Cinder libs relative in Visual Studio properties

--- samples/SimpleVideoTexture/src/SimpleVideoTextureApp.cpp
+++ samples/SimpleVideoTexture/src/SimpleVideoTextureApp.cpp
@@ -12,9 +12,9 @@ public:
 	~SimpleVideoTextureApp();
 
 	void	draw() override;
-	void	mouseDown( ci::app::MouseEvent event ) override;
-	void	mouseDrag( ci::app::MouseEvent event ) override;
-	void	mouseUp( ci::app::MouseEvent event ) override;
+	void	mouseDown(ci::app::MouseEvent event) override;
+	void	mouseDrag(ci::app::MouseEvent event) override;
+	void	mouseUp(ci::app::MouseEvent event) override;
 	void	resize() override;
 	void	setup() override;
 	void	update() override;
@@ -52,23 +52,23 @@ using namespace ci::app;
 using namespace std;
 
 SimpleVideoTextureApp::SimpleVideoTextureApp() : 
-	mVideoPos( 0.0f ), 
-	mVideoDuration( 0.0f ), 
-	mIsVideoLoaded( false ), 
-	mFps( 0.0f ), 
-	mIsFullScreen( false )
+	mVideoPos(0.0f), 
+	mVideoDuration(0.0f), 
+	mIsVideoLoaded(false), 
+	mFps(0.0f), 
+	mIsFullScreen(false)
 {
 	loadShaders();
 
-	mBatchWirePlane = gl::Batch::create( geom::WirePlane().normal( vec3( 0, 0, 1 ) ).subdivisions( vec2( 3 ) ), mGlslStockColor );
-	mBatchPlaneNoise = gl::Batch::create( geom::Plane().normal( vec3( 0, 0, 1 ) ), mGlslTvNoise );
-	mBatchPlaneVideo = gl::Batch::create( geom::Plane().normal( vec3( 0, 0, 1 ) ), mGlslVideoTexture );
+	mBatchWirePlane = gl::Batch::create(geom::WirePlane().normal(vec3(0, 0, 1)).subdivisions(vec2(3)), mGlslStockColor);
+	mBatchPlaneNoise = gl::Batch::create(geom::Plane().normal(vec3(0, 0, 1)), mGlslTvNoise);
+	mBatchPlaneVideo = gl::Batch::create(geom::Plane().normal(vec3(0, 0, 1)), mGlslVideoTexture);
 
-	mCam.setPerspective( 60.0f, getWindowAspectRatio(), 0.01f, 10000.0f );
-	mCam.lookAt( vec3( 0, 0, 500 ), vec3(), vec3( 0, 1, 0 ) );
+	mCam.setPerspective(60.0f, getWindowAspectRatio(), 0.01f, 10000.0f);
+	mCam.lookAt(vec3(0, 0, 500), vec3(), vec3(0, 1, 0));
 
-	mCamUi.setCamera( &mCam );
-	mCamUi.setMouseWheelMultiplier( -mCamUi.getMouseWheelMultiplier() );
+	mCamUi.setCamera(&mCam);
+	mCamUi.setMouseWheelMultiplier(-mCamUi.getMouseWheelMultiplier());
 }
 
 SimpleVideoTextureApp::~SimpleVideoTextureApp()
@@ -77,53 +77,53 @@ SimpleVideoTextureApp::~SimpleVideoTextureApp()
 
 void SimpleVideoTextureApp::draw()
 {
-	gl::ScopedViewport scopedViewport( getWindowSize() );
-	gl::ScopedDepth scopedDepth( true );
+	gl::ScopedViewport scopedViewport(getWindowSize());
+	gl::ScopedDepth scopedDepth(true);
 	gl::ScopedMatrices scopedMatrices;
 	
-	gl::setMatrices( mCam );
-	gl::clear( Color( 0, 0, 0 ) );
+	gl::setMatrices(mCam);
+	gl::clear(Color(0, 0, 0));
 
-	if ( mIsVideoLoaded ) {
-		vec2 videoSize = vec2( mVideo.getWidth(), mVideo.getHeight() );
-		mGlslVideoTexture->uniform( "uVideoSize", videoSize );
+	if (mIsVideoLoaded) {
+		vec2 videoSize = vec2(mVideo.getWidth(), mVideo.getHeight());
+		mGlslVideoTexture->uniform("uVideoSize", videoSize);
 		videoSize *= 0.25f;
 		videoSize *= 0.5f;
 
 		{
-			gl::ScopedColor scopedColor( Colorf( 1, 0, 1 ) );
+			gl::ScopedColor scopedColor(Colorf(1, 0, 1));
 			gl::ScopedModelMatrix scopedModelMatrix;
 
-			gl::translate( vec3( 0, 0, 1.0f ) );
-			gl::scale( vec3( videoSize, 1.0f ) );
+			gl::translate(vec3(0, 0, 1.0f));
+			gl::scale(vec3(videoSize, 1.0f));
 			mBatchWirePlane->draw();
 		}
 
 		{
-			gl::ScopedColor scopedColor( Colorf::white() );
+			gl::ScopedColor scopedColor(Colorf::white());
 			gl::ScopedModelMatrix scopedModelMatrix;
 
-			ciWMFVideoPlayer::ScopedVideoTextureBind scopedVideoTex( mVideo, 0 );
+			ciWMFVideoPlayer::ScopedVideoTextureBind scopedVideoTex(mVideo, 0);
 
-			gl::scale( vec3( videoSize, 1.0f ) );
+			gl::scale(vec3(videoSize, 1.0f));
 			mBatchPlaneVideo->draw();
 		}
 	} else {
 		{
-			gl::ScopedColor scopedColor( Colorf( 1, 0, 1 ) );
+			gl::ScopedColor scopedColor(Colorf(1, 0, 1));
 			gl::ScopedModelMatrix scopedModelMatrix;
 
-			gl::translate( vec3( 0, 0, 1 ) );
-			gl::scale( vec3( 320, 180, 1 ) );
+			gl::translate(vec3(0, 0, 1));
+			gl::scale(vec3(320, 180, 1));
 			mBatchWirePlane->draw();
 		}
 
 		{
 			gl::ScopedModelMatrix scopedModelMatrix;
 
-			mGlslTvNoise->uniform( "uTime", static_cast<float>( getElapsedSeconds() ) );
+			mGlslTvNoise->uniform("uTime", static_cast<float>(getElapsedSeconds()));
 
-			gl::scale( vec3( 320, 180, 1 ) );
+			gl::scale(vec3(320, 180, 1));
 			mBatchPlaneNoise->draw();
 		}
 	}
@@ -133,66 +133,66 @@ void SimpleVideoTextureApp::draw()
 
 void SimpleVideoTextureApp::loadShaders()
 {
-	mGlslStockColor = gl::getStockShader( gl::ShaderDef().color() );
+	mGlslStockColor = gl::getStockShader(gl::ShaderDef().color());
 
 	try {
-		mGlslVideoTexture = gl::GlslProg::create( gl::GlslProg::Format()
-			.vertex( loadAsset( "video_texture.vs.glsl" ) ) 
-			.fragment( loadAsset( "video_texture.fs.glsl" ) ) );
-	} catch ( gl::GlslProgCompileExc ex ) {
-		CI_LOG_E( "<< GlslProg Compile Error >>\n" << ex.what() );
-	} catch ( gl::GlslProgLinkExc ex ) {
-		CI_LOG_E( "<< GlslProg Link Error >>\n" << ex.what() );
-	} catch ( gl::GlslProgExc ex ) {
-		CI_LOG_E( "<< GlslProg Error >> " << ex.what() );
-	} catch ( AssetLoadExc ex ) {
-		CI_LOG_E( "<< Asset Load Error >> " << ex.what() );
+		mGlslVideoTexture = gl::GlslProg::create(gl::GlslProg::Format()
+			.vertex(loadAsset("video_texture.vs.glsl")) 
+			.fragment(loadAsset("video_texture.fs.glsl")));
+	} catch (gl::GlslProgCompileExc ex) {
+		CI_LOG_E("<< GlslProg Compile Error >>\n" << ex.what());
+	} catch (gl::GlslProgLinkExc ex) {
+		CI_LOG_E("<< GlslProg Link Error >>\n" << ex.what());
+	} catch (gl::GlslProgExc ex) {
+		CI_LOG_E("<< GlslProg Error >> " << ex.what());
+	} catch (AssetLoadExc ex) {
+		CI_LOG_E("<< Asset Load Error >> " << ex.what());
 	}
 
 	try {
-		mGlslTvNoise = gl::GlslProg::create( gl::GlslProg::Format()
-			.vertex( loadAsset( "tv_noise.vs.glsl" ) ) 
-			.fragment( loadAsset( "tv_noise.fs.glsl" ) ) );
-	} catch ( gl::GlslProgCompileExc ex ) {
-		CI_LOG_E( "<< GlslProg Compile Error >>\n" << ex.what() );
-	} catch ( gl::GlslProgLinkExc ex ) {
-		CI_LOG_E( "<< GlslProg Link Error >>\n" << ex.what() );
-	} catch ( gl::GlslProgExc ex ) {
-		CI_LOG_E( "<< GlslProg Error >> " << ex.what() );
-	} catch ( AssetLoadExc ex ) {
-		CI_LOG_E( "<< Asset Load Error >> " << ex.what() );
+		mGlslTvNoise = gl::GlslProg::create(gl::GlslProg::Format()
+			.vertex(loadAsset("tv_noise.vs.glsl")) 
+			.fragment(loadAsset("tv_noise.fs.glsl")));
+	} catch (gl::GlslProgCompileExc ex) {
+		CI_LOG_E("<< GlslProg Compile Error >>\n" << ex.what());
+	} catch (gl::GlslProgLinkExc ex) {
+		CI_LOG_E("<< GlslProg Link Error >>\n" << ex.what());
+	} catch (gl::GlslProgExc ex) {
+		CI_LOG_E("<< GlslProg Error >> " << ex.what());
+	} catch (AssetLoadExc ex) {
+		CI_LOG_E("<< Asset Load Error >> " << ex.what());
 	}
 
-	if ( mBatchWirePlane ) {
-		mBatchWirePlane->replaceGlslProg( mGlslStockColor );
+	if (mBatchWirePlane) {
+		mBatchWirePlane->replaceGlslProg(mGlslStockColor);
 	}
 
-	if ( mBatchPlaneVideo ) {
-		mBatchPlaneVideo->replaceGlslProg( mGlslVideoTexture );
+	if (mBatchPlaneVideo) {
+		mBatchPlaneVideo->replaceGlslProg(mGlslVideoTexture);
 	}
 
-	if ( mBatchPlaneNoise ) {
-		mBatchPlaneNoise->replaceGlslProg( mGlslTvNoise );
+	if (mBatchPlaneNoise) {
+		mBatchPlaneNoise->replaceGlslProg(mGlslTvNoise);
 	}
 }
 
-void SimpleVideoTextureApp::mouseDown( MouseEvent event )
+void SimpleVideoTextureApp::mouseDown(MouseEvent event)
 {
-	mCamUi.mouseDown( event );
+	mCamUi.mouseDown(event);
 }
 
-void SimpleVideoTextureApp::mouseDrag( MouseEvent event )
+void SimpleVideoTextureApp::mouseDrag(MouseEvent event)
 {
-	mCamUi.mouseDrag( event );
+	mCamUi.mouseDrag(event);
 }
 
-void SimpleVideoTextureApp::mouseUp( MouseEvent event )
+void SimpleVideoTextureApp::mouseUp(MouseEvent event)
 {
 }
 
 void SimpleVideoTextureApp::resize()
 {
-	mCam.setAspectRatio( getWindowAspectRatio() );
+	mCam.setAspectRatio(getWindowAspectRatio());
 }
 
 void SimpleVideoTextureApp::setup()
@@ -207,34 +207,34 @@ void SimpleVideoTextureApp::setup()
 		return mIsFullScreen;
 	};
 
-	auto setIsFullScreen = [ & ]( bool value ) -> void
+	auto setIsFullScreen = [ & ](bool value) -> void
 	{
 		mIsFullScreen = value;
-		setFullScreen( mIsFullScreen );
+		setFullScreen(mIsFullScreen);
 	};
 
 	auto loadVideo = [ & ]() -> void
 	{
-		fs::path videoPath = getOpenFilePath( fs::path(), { "mp4", "wmv", "avi", "mov" } );
-		if ( !videoPath.empty() ) {
-			if ( !mVideo.isStopped() ) {
+		fs::path videoPath = getOpenFilePath(fs::path(), { "mp4", "wmv", "avi", "mov" });
+		if (!videoPath.empty()) {
+			if (!mVideo.isStopped()) {
 				mVideo.stop();
 			}
 
-			mIsVideoLoaded = mVideo.loadMovie( videoPath );
+			mIsVideoLoaded = mVideo.loadMovie(videoPath);
 			mVideoDuration = mVideo.getDuration();
 			mVideoPos = mVideo.getPosition();
 			mVideo.play();
 		}
 	};
 
-	mParams = params::InterfaceGl::create( "Params", ivec2( 210, 210 ) );
-	mParams->addParam<float>( "FPS", &mFps, true );
-	mParams->addParam<bool>( "Full Screen", setIsFullScreen, getIsFullScreen ).key( "F" );
-	mParams->addParam<float>( "Video duration", &mVideoDuration, true );
-	mParams->addParam<float>( "Video position", &mVideoPos, true );
-	mParams->addButton( "Reload shaders", reloadShaders, "key=R" );
-	mParams->addButton( "Load video", loadVideo, "key=V" );
+	mParams = params::InterfaceGl::create("Params", ivec2(210, 210));
+	mParams->addParam<float>("FPS", &mFps, true);
+	mParams->addParam<bool>("Full Screen", setIsFullScreen, getIsFullScreen).key("F");
+	mParams->addParam<float>("Video duration", &mVideoDuration, true);
+	mParams->addParam<float>("Video position", &mVideoPos, true);
+	mParams->addButton("Reload shaders", reloadShaders, "key=R");
+	mParams->addButton("Load video", loadVideo, "key=V");
 }
 
 void SimpleVideoTextureApp::update()
@@ -244,8 +244,8 @@ void SimpleVideoTextureApp::update()
 	mVideoPos = mVideo.getPosition();
 }
 
-CINDER_APP( SimpleVideoTextureApp, RendererGl, []( App::Settings * settings )
+CINDER_APP(SimpleVideoTextureApp, RendererGl, [](App::Settings * settings)
 {
-	settings->setFrameRate( 60.0f );
-	settings->prepareWindow( Window::Format().size( 1280, 720 ).title( "Simple Video Texture Test" ) );
-} )
+	settings->setFrameRate(60.0f);
+	settings->prepareWindow(Window::Format().size(1280, 720).title("Simple Video Texture Test"));
+})
